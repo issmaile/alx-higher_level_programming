a076db0b034404a0bc44c85785e4fafc1c65724f
issmaile <ismail25smello@gmail.com>
2023-08-10 19:20:42 +0100

    4-hidden_discovery.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 
-import sys
+if __name__ == "__main__":
+    import sys
 
-argc = len(sys.argv) - 1
-args = sys.argv[1:]
+    argc = len(sys.argv) - 1
 
-if argc == 0:
-    print("0 arguments.")
-elif argc == 1:
-    print("1 argument:")
-    print("1: {}".format(args[0]))
-else:
-    print("{} arguments:".format(argc))
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(argc))
+
     for i in range(argc):
-        print("{}: {}".format(i + 1, args[i]))
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
