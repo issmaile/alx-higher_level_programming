21fb1b915753c0c6053bf906a0bf4522233f2e63
issmaile <ismail25smello@gmail.com>
2023-09-12 01:17:55 +0100

    2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a """
+
+
+def is_same_class(obj, a_class):
+    """ asdasfasfd
+
+    Args:
+        obj (any): asdasd
+        a_class (type): asdasd
+    Returns:
+        asdasda
+    """
+    if type(obj) == a_class:
+        return True
+    return False
