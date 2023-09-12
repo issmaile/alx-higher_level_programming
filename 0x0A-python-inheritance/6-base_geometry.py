3ad718e46f10d16b854735583e4d0e08599aeb32
issmaile <ismail25smello@gmail.com>
2023-09-12 01:37:29 +0100

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""asdasdasfdasfadfgsdfghsfgh Goe"""
+
+
+class BaseGeometry:
+    """Represent base geometry"""
+
+    def area(self):
+        """ASdfadfadf"""
+        raise Exception("area() is not implemented")
