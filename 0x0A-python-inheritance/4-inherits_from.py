6904b65ef3e10ccaa1a4b5349615785a4703efed
issmaile <ismail25smello@gmail.com>
2023-09-12 01:34:31 +0100

    4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Definasdes a """
+
+
+def inherits_from(obj, a_class):
+    """ asdasfasfasdasdd
+
+    Args:
+        obj (any): asdaasdassd
+        a_class (type): asdaasfdadfsd
+    Returns:
+        asdasdaasdaf
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
