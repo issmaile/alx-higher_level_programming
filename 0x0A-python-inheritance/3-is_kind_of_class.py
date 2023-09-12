2898218d5831cc5f7cbecc5d2eee492159149eea
issmaile <ismail25smello@gmail.com>
2023-09-12 01:30:19 +0100

    3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a """
+
+
+def is_kind_of_class(self, a_class):
+    """ asdasfasfd
+
+    Args:
+        obj (any): asdasd
+        a_class (type): asdasd
+    Returns:
+        asdasda
+    """
+    if isinstance(obj, a_class):
+        return True
+    return False
