a380eb81a8bb191d55e2eb9cc3e6228ce9eb10f0
issmaile <ismail25smello@gmail.com>
2023-12-02 03:03:52 +0100

    .

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -2,11 +2,13 @@
 """displays X-Request-Id of a requst of a given URL
 """
 import sys
-import urllib.request
+import requests
 
 if __name__ == "__main__":
     url = sys.argv[1]
+    email = sys.argv[2]
+    data = {'email' : email}
 
-    req = urllib.request.Request(url)
-    with urllib.request.urlopen(req) as res:
-        print(dict(res.headers).get("X-Request-Id"))
+    res = requests.post(url, data=data)
+    print("Your email is: {}".format(email))
+    print(response.text)
