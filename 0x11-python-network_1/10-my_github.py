ec6f2734620bff7fcb42085a52630ed9ebac4340
issmaile <ismail25smello@gmail.com>
2023-12-02 03:10:37 +0100

    .

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""displays ur id by taking
+- your GitHub credentials (username and password)
+"""
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    req = requests.get("https://api.github.com/user", auth=auth)
+    print(req.json().get("id"))
