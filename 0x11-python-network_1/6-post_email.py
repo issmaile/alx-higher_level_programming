4a419c989bacdee84d0a41f140c3ee9c46d42071
issmaile <ismail25smello@gmail.com>
2023-12-06 03:30:15 +0100

    .

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,7 +7,7 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    data = {'email' : email}
+    data = {'email': email}
 
     res = requests.post(url, data=data)
     print("Your email is: {}".format(email))
