76283c4fa0de7ea962336f76706bde4395788b8b
issmaile <ismail25smello@gmail.com>
2024-01-03 22:56:58 +0100

    1-writeme.js

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const fs = require('fs');
+const file = process.argv(2);
+const content = process.argv(3);
+try {
+  fs.writeFileSync(file, content, 'utf-8');
+} catch (err) {
+  console.log(err);
+}
