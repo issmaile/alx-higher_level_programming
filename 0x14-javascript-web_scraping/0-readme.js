8193e85d90dce1db4c2a5b1cc7eb49c615aff666
issmaile <ismail25smello@gmail.com>
2024-01-03 22:52:21 +0100

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
 try {
-	const data = fs.readFileSync(process.argv[2], 'utf8');
-	console.log(data);
-}
-catch (err) {
-	console.log(err);
+  const data = fs.readFileSync(process.argv[2], 'utf8');
+  console.log(data);
+} catch (err) {
+  console.log(err);
 }
