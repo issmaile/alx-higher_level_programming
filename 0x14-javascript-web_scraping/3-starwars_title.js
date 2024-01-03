23aae5fd2ddc3b082f1913f3db70e374f30b170e
issmaile <ismail25smello@gmail.com>
2024-01-03 23:14:42 +0100

    3-starwars_title.js

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const request = require('request');
+const id = process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + id;
+
+request(url, (err, resp) => {
+  if (err) throw (err);
+  console.log(resp.title);
+}
