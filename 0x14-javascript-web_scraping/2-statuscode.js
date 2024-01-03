9fe3a8387cc0531e0691c1d5ec36c01a29de37ed
issmaile <ismail25smello@gmail.com>
2024-01-03 23:09:28 +0100

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const req = require('request');
-const url = process.argv[2]
+const url = process.argv[2];
 
 req(url, (err, res) => {
-  if (err) throw(err);
+  if (err) throw (err);
   console.log('code:', res.statusCode);
 });
