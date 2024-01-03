4abab2d2d8a2c5ac453f155e2151bac439e1e9b8
issmaile <ismail25smello@gmail.com>
2024-01-03 23:52:17 +0100

    6-completed_tasks.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const req = require('request');
+const url = process.argv[2];
+
+req(url, (err, resp, body) => {
+  if (err) throw (err);
+  const users = {};
+  for (const task of body) {
+    if (task.completed) {
+      users[task.userId] = (users[task.userId] || 0) + 1;
+    }
+  }
+  console.log(users);
+});
