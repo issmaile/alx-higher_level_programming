15bd9d6f03795027c7212cb6a8a8a45383adc3a7
issmaile <ismail25smello@gmail.com>
2024-01-03 23:29:31 +0100

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,5 +3,6 @@ const req = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/people/18';
 
 req(url, { json: true }, (err, resp, body) => {
+  if (err) throw (err);
   console.log(body.films.length);
 });
